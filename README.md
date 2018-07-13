a70a28fdc4f69d2fd13e9096855ac80fb3c8e098
Arunsingh <arun@Arunsinghs-MBP.home>
2018-07-13 02:20:03 -0700

    adding s3 bucket policy module

--- README.md
+++ README.md
@@ -3,13 +3,30 @@
 The Idea of this repo is to write a wrapper functionality for frequently used aws services
 The idea of Devops and Automating infrastructure in the public cloud space has boiled down to the point of lifting heavy tasks to much simpler libraries
 
+*******IAM*******
+
 Creating IAM Role and Attaching custom managed Policy to the IAM Role
 
 Note:  Do not remove the mandate_iam_assume.json as it provides the AssumeRolePolicyDocument (change as needed for other entities like lambda)
+
 Step 1: git clone this repository
 
 Step 2: virtualenv aws-python-boto3
 
 Step 3: source aws-python-boto3/bin/activate
 
-Step 4: Run python iam/role.py [ROLENAME] [POLICYNAME] [CUSTOM JSON POLICY DOCUMENT]
+Step 4: Run  python  iam/roles.py  [ROLENAME]  [POLICYNAME]  [CUSTOM JSON POLICY DOCUMENT]
+
+Example: python iam/roles.py samplerole samplepolicy sample_s3.json
+
+*********S3 BUCKET POLICY ATTACHMENT TO BUCKETS***************
+
+s3_bucketpolicy:
+
+Creating custom Bucket policies and attaching to list of S3 Buckets
+
+Change the BucketList file and Policy as per need - This is an example code
+
+Run  python  s3_bucketpolicy/s3_bucket_policy.py [BUCKET_LIST_FILE] [BUCKET_POLICY_JSON]
+
+Example: python s3_bucketpolicy/s3_bucket_policy.py bucketlist bucket_policy.json
---
+++ s3_bucketpolicy/s3_bucket_policy.py
@@ -0,0 +1,49 @@
+import json
+import boto3
+import sys
+import logging
+
+
+class bucketpolicy:
+    def __init__(self, buckets, policys):
+        self.buckets = buckets
+        self.policy = policys
+    def attach_bucketpolicies(self):
+        s3 = boto3.client('s3')
+        try:
+            #store the policydata and iterate to append the bucket names in the bucket policys
+            with open(self.policy, 'r') as bucket_policy:
+                policy_data = json.load(bucket_policy)
+            tmp = policy_data['Statement'][0]['Resource']
+            #Iterate through the buckets and add the policies as per the bucket name
+            with open(self.buckets, 'r') as bucket_list:
+                s3_list = [line.rstrip('\n') for line in bucket_list]
+            for name in s3_list:
+                policy_data['Statement'][0]['Resource'] = 'arn:aws:s3:::{}'.format(name)
+                bucket_policy = json.dumps(policy_data)
+                s3.put_bucket_policy(Bucket=name, Policy=bucket_policy)
+        except Exception as e:
+            logging.info('Error in attaching policies to the bucketsv{}'.format(e))
+
+    def revert_json(self):
+        try:
+            with open(self.policy, 'r') as revert:
+                policy_data = json.load(revert)
+            tmp = policy_data['Statement'][0]['Resource']
+            policy_data['Statement'][0]['Resource'] = 'arn:aws:s3:::'
+            with open(self.policy, 'r') as revert:
+                json.dump(policy_data)
+            logging.info('Execution of Bucket Policy attachment to the buckets in file {} success'.format(self.buckets))
+        except Exception as e:
+            logging.info('Error in reverting the json{}'.format(e))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print ('Error. Provide the arguments - bucketlist_filename, policy_filename')
+        exit()
+    buckets = sys.argv[1]
+    policys = sys.argv[2]
+    bucket_policy = bucketpolicy(buckets, policys)
+    if bucket_policy.attach_bucketpolicies():
+        bucket_policy.revert_json()
